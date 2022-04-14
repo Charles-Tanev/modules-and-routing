0be2dca8ff49281fcf523d1ac05478d5588b532e
ELMIR TANEV <charles.tanev@gmail.com>
2022-04-14 10:23:27 +0300

    init
    init

--- modules-and-routing-app/src/app/app.module.ts
+++ modules-and-routing-app/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { CustomerModule } from './customer/customer.module';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    CustomerModule
+    CustomerModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
